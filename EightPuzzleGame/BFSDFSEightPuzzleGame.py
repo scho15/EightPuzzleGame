f22f2ae58e3081f2220b948b2a92720e1c1782e8
Simon Schofield <Simon Schofield>
2017-06-04 17:10:21 +0100

    Add project files.

---
+++ EightPuzzleGame/BFSDFSEightPuzzleGame.py
@@ -0,0 +1,124 @@
+import queue
+import time
+import sys
+
+def GraphSearch(initialState):
+
+    start_time = time.time()
+    bfsFrontier = queue.Queue()
+    bfsPath = queue.Queue()
+    explored = set()
+    state = tuple()
+    frontierSet = set()
+    neighbour = list()
+    initialPath = list()
+    currentPath = list()
+    nodesExpanded = 0
+    maxDepth = 0
+    maxDepthIndicator = bool()
+
+    bfsFrontier.put(initialState)
+    frontierSet.add(tuple(initialState))
+    goalTest = (0,1,2,3,4,5,6,7,8)
+
+    while bfsFrontier.empty() == False:        
+        ## print ("Length of bfsFrontier before expansion is ", len(bfsFrontier))
+        state = bfsFrontier.get()
+        frontierSet.remove(state)
+        if bfsPath.empty() == False:
+            initialPath = bfsPath.get()
+
+        if goalTest == tuple(state):
+            print("path_to_goal:",initialPath)
+            print("cost_of_path:", len(initialPath))
+            print("nodes_expanded:", len(explored))
+            print("search_depth:", len(initialPath))
+            print("max_search_depth:", maxDepth)
+            print("running_time:", round(time.time() - start_time,8))
+            print("max_ram_usage:", round(SystemResource(),8))
+            return 
+
+        explored.add(tuple(state))
+
+		## Determine Up neighbour
+        if (state.index(0) > 2):
+            neighbour = list(state)
+            currentPath = list(initialPath)
+            a, b = neighbour.index(0),neighbour.index(0) - 3
+            neighbour[b], neighbour[a] = neighbour[a], neighbour[b]
+            ## print ("Up", neighbour)
+            if tuple(neighbour) not in list(explored) and tuple(neighbour) not in list(frontierSet):
+                bfsFrontier.put(tuple(neighbour))
+                frontierSet.add(tuple(neighbour))
+                currentPath.append("Up")
+                bfsPath.put(list(currentPath))
+            if len(currentPath) > maxDepth:
+                maxDepth = len(currentPath)
+                maxDepthIndicator = True
+		## Determine Down neighbour
+        if (state.index(0) < 6):
+            neighbour = list(state)
+            currentPath = list(initialPath)
+            a, b = neighbour.index(0),neighbour.index(0) + 3
+            neighbour[b], neighbour[a] = neighbour[a], neighbour[b]
+            ## print ("Down", neighbour)
+            if tuple(neighbour) not in list(explored) and tuple(neighbour) not in list(frontierSet):
+                bfsFrontier.put(tuple(neighbour))
+                frontierSet.add(tuple(neighbour))
+                currentPath.append("Down")
+                bfsPath.put(list(currentPath))
+            if len(currentPath) > maxDepth:
+                maxDepth = len(currentPath)
+                maxDepthIndicator = True
+		## Determine Left neighbour
+        if (state.index(0) % 3 != 0):
+            neighbour = list(state)
+            currentPath = list(initialPath)
+            a, b = neighbour.index(0),neighbour.index(0) - 1
+            neighbour[b], neighbour[a] = neighbour[a], neighbour[b]
+            ## print ("Left", neighbour)
+            if tuple(neighbour) not in list(explored) and tuple(neighbour) not in list(frontierSet):
+                bfsFrontier.put(tuple(neighbour))
+                frontierSet.add(tuple(neighbour))
+                currentPath.append("Left")
+                bfsPath.put(list(currentPath))
+            if len(currentPath) > maxDepth:
+                maxDepth = len(currentPath)
+                maxDepthIndicator = True
+		## Determine Right neighbour
+        if (state.index(0) % 3 != 2):
+            neighbour = list(state)
+            currentPath = list(initialPath)
+            a, b = neighbour.index(0),neighbour.index(0) + 1
+            neighbour[b], neighbour[a] = neighbour[a], neighbour[b]
+            ## print ("Right", neighbour)
+            if tuple(neighbour) not in list(explored) and tuple(neighbour) not in list(frontierSet):
+                bfsFrontier.put(tuple(neighbour))
+                frontierSet.add(tuple(neighbour))
+                currentPath.append("Right")
+                bfsPath.put(list(currentPath))
+            if len(currentPath) > maxDepth:
+                maxDepth = len(currentPath)
+                maxDepthIndicator = True
+        nodesExpanded += 1
+        if maxDepthIndicator:
+            print("maxDepth:", maxDepth, "Frontier Size", bfsFrontier.qsize(), "nodesExpanded", nodesExpanded)
+        maxDepthIndicator = False
+    print(initialState)
+    return
+
+def SystemResource():
+    if sys.platform == "win32" or sys.platform == "win64":
+        import psutil
+        return psutil.Process().memory_info().rss/1000000000
+    else:
+    # Note: if you execute Python from cygwin,
+    # the sys.platform is "cygwin"
+    # the grading system's sys.platform is "linux2"
+        import resource
+        return resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1000
+
+## GraphSearch((1,2,5,3,4,0,6,7,8))
+GraphSearch((6,1,8,4,0,2,7,3,5))
+## GraphSearch((8,6,4,2,1,3,5,7,0))
+## GraphSearch((3,1,2,0,4,5,6,7,8))
